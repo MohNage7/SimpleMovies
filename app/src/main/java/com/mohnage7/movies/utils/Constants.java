b8fe1d13911e3cb17750bdcacfe0bba86100bf4a
Mohamed Nageh <nageh@zadfresh.com>
2019-10-06 01:37:14 +0200

    - Removed keys

--- app/src/main/java/com/mohnage7/movies/utils/Constants.java
+++ app/src/main/java/com/mohnage7/movies/utils/Constants.java
@@ -9,11 +9,11 @@ import static java.lang.annotation.RetentionPolicy.SOURCE;
 public class Constants {
     public static final String BASE_URL = "https://api.themoviedb.org/3/";
     public static final String MOVIE_EXTRA = "movie_extra";
-    public static final String API_KEY = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYzgwNDFkZmUyYmU1MWRiNTJlMGZmYWFkYWJlYmM4ZCIsInN1YiI6IjVhOTcyMzQwMGUwYTI2MDE1ZjAwNTMyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sogWARNxbqkf9fduFd1vetw8KUyDn5xY1Bb_4sVJS1o";
+    public static final String API_KEY = "";
     public static final String POPULAR = "popular";
     public static final String TOP_RATED = "top_rated";
     public static final String UP_COMING = "upcoming";
-    public static final String YOUTUBE_KEY = "AIzaSyDTsUcgRF_0mn-6bUoRRB3jqvoL0iPl85k";
+    public static final String YOUTUBE_KEY = "";
     public static final int CONNECTION_TIMEOUT = 10; // 10 seconds
     public static final int READ_TIMEOUT = 2; // 2 seconds
     public static final int WRITE_TIMEOUT = 2; // 2 seconds
